3f88d2afc84f16ed090190231acb9d8590e8e515
royer <royerwang@gmail.com>
2022-05-25 06:04:39 -0700

    Android.checkPermission

---
+++ android/src/main/kotlin/com/royzed/simple_bg_location/permission/LocationPermission.kt
@@ -0,0 +1,9 @@
+package com.royzed.simple_bg_location.permission;
+
+public enum LocationPermission {
+    denied,
+    deniedForever,
+    whileInUse,
+    always,
+    unableToDetermine
+}
---
+++ android/src/main/kotlin/com/royzed/simple_bg_location/permission/PermissionManager.kt
@@ -0,0 +1,4 @@
+package com.royzed.simple_bg_location.permission
+
+class PermissionManager {
+}

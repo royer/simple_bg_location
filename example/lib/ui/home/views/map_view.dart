a941977a84800bb4921c18805ee2a8dcba3c336f
royer <royerwang@gmail.com>
2022-06-10 22:21:16 -0700

    position update on MapView

--- example/lib/ui/home/views/map_view.dart
+++ example/lib/ui/home/views/map_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:simple_bg_location/simple_bg_location.dart';
 import 'dart:developer' as dev;
 import 'package:simple_bg_location_example/cubit/position_cubit.dart';
 
@@ -19,7 +20,10 @@ class _MapViewState extends State<MapView> with AutomaticKeepAliveClientMixin {
 
   late MapOptions _mapOptions;
 
-  List<LatLng> _trace = [];
+  final List<LatLng> _trace = [];
+
+  final List<CircleMarker> _positions = [];
+
   @override
   void initState() {
     super.initState();
@@ -43,6 +47,18 @@ class _MapViewState extends State<MapView> with AutomaticKeepAliveClientMixin {
       listener: (context, state) {
         if (state is PositionArrived) {
           final position = state.newPosition;
+          _onNewPositionArrived(position);
+        } else if (state is PositionUpdateState) {
+          if (state.isTracking) {
+            if (state.positions.isNotEmpty) {
+              for (final position in state.positions) {
+                _onNewPositionArrived(position);
+              }
+            }
+          } else {
+            _clear();
+            setState(() {});
+          }
         }
       },
       child: FlutterMap(
@@ -66,6 +82,11 @@ class _MapViewState extends State<MapView> with AutomaticKeepAliveClientMixin {
               color: const Color.fromRGBO(0, 179, 253, 0.8),
             ),
           ])),
+          CircleLayerWidget(
+            options: CircleLayerOptions(
+              circles: _positions,
+            ),
+          ),
         ],
       ),
     );
@@ -73,6 +94,26 @@ class _MapViewState extends State<MapView> with AutomaticKeepAliveClientMixin {
 
   void _onPositionChanged(MapPosition pos, bool hasGesture) {
     _mapOptions.crs.scale(_mapController.zoom);
+  }
+
+  void _onNewPositionArrived(Position position) {
+    LatLng ll = LatLng(position.latitude, position.longitude);
+    _mapController.move(ll, _mapController.zoom);
+
+    _trace.add(ll);
+
+    //_positions.add(CircleMarker(point: ll, radius: 5.0, color: Colors.black));
+    _positions.add(CircleMarker(
+        point: ll,
+        radius: 3.0,
+        color: Colors.amber,
+        borderColor: Colors.black,
+        borderStrokeWidth: 1));
+  }
+
+  void _clear() {
+    _trace.clear();
+    _positions.clear();
   }
 
   @override

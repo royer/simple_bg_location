82f22c4c9735adaa094766d4b503f0b234dc5f6f
royer <royerwang@gmail.com>
2023-03-04 22:24:14 -0800

    upgrade some dependency

--- lib/src/errors/errors.dart
+++ lib/src/errors/errors.dart
@@ -1,7 +1,6 @@
 export 'location_permission_exception.dart';
 export 'permission_definitions_not_found_exception.dart';
 export 'permission_denied_exception.dart';
-export 'location_permission_exception.dart';
 export 'location_accuracy_permission_exception.dart';
 export 'location_services_disabled_exception.dart';
 export "location_accuracy_value_exception.dart";
